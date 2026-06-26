89222f801661e4b146bc78c775a17fd7a0ba7306
Arenef <baokhangcoder@gmail.com>
2026-06-26 21:16:56 +0700

    Update ida_star.py

--- AppVaccum/ida_star.py
+++ AppVaccum/ida_star.py
@@ -21,12 +21,11 @@ class ida_star_vacuum:
     def __init__(self):
         state =[
                 [0, 0, 1, 1],
-                [0, 2, -1, 1],
-                [1, -1, 1, 1],
-                [1, 0, 0, 1]]
+                [0, 2, -1, 0],
+                [1, -1, 0, 0],
+                [0, 0, 0, 1]]
         self.start = state
         self.counter = 0
-
 
     def get_location(self, node):
         matrix = node.state
@@ -87,34 +86,40 @@ class ida_star_vacuum:
         return True
     
     def solve(self):
+        self.search_events = []
         I = self.heuristic(self.start)
         
         while True:
+            self.search_events.append((self.start, f"\n--- IDA* Lượt Tìm Kiếm (Ngưỡng I = {I}) ---"))
             node, alpha = self.search(I)
             
             if node != None:
                 return node
             
+            self.search_events.append((self.start, f"Không tìm thấy giải pháp dưới ngưỡng I = {I}. Tăng ngưỡng I thêm {alpha}"))
             I += alpha
 
             if I == float("inf"):
                 return None
 
     def search(self, I):
-    
         frontier = []
         start_node = Node(self.counter, self.start, None, "START", self.heuristic(self.start), 0)
         heapq.heappush(frontier, start_node)
+        self.search_events.append((start_node.state, f"  Thêm node khởi đầu vào frontier: Vị trí {self.get_location(start_node)}, f(n): {start_node.cost_path} (g: {start_node.g}, h: {start_node.cost_path - start_node.g})"))
+        
         self.reached = set()
         self.reached.add(self.matrix_to_tuple(self.start))
         alpha = 10e9
         while frontier:
             node = heapq.heappop(frontier)
+            self.search_events.append((node.state, f"  Lấy node khỏi frontier: Vị trí {self.get_location(node)}, Hành động: {node.act}, f(n): {node.cost_path} (g: {node.g}, h: {node.cost_path - node.g})"))
 
             if node.cost_path < alpha:
                alpha = node.cost_path
 
             if self.is_goal(node):
+                self.search_events.append((node.state, f"  -> Đạt trạng thái đích ở vị trí: {self.get_location(node)}"))
                 return node, alpha
             
             move = self.possible_move(node)
@@ -122,9 +127,14 @@ class ida_star_vacuum:
             for m in move:
                 new_node = self.act(node, m)
 
-                if self.matrix_to_tuple(new_node.state) not in self.reached and new_node.cost_path < I:
+                if new_node.cost_path >= I:
+                    self.search_events.append((new_node.state, f"  -> Từ chối láng giềng: Vị trí {self.get_location(new_node)} (f(n): {new_node.cost_path} >= giới hạn I: {I})"))
+                elif self.matrix_to_tuple(new_node.state) in self.reached:
+                    pass
+                else:
                     self.reached.add(self.matrix_to_tuple(new_node.state))
                     heapq.heappush(frontier, new_node)
+                    self.search_events.append((new_node.state, f"  -> Thêm node vào frontier: Vị trí {self.get_location(new_node)}, Hành động: {new_node.act}, f(n): {new_node.cost_path} (g: {new_node.g}, h: {new_node.cost_path - new_node.g})"))
 
         return None, alpha
     
